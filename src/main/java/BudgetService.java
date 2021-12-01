19ee71a3a1d4fac664286d3064080f2d4d05e6af
Darren_Chen <darren_chen@trend.com.tw>
2021-12-01 16:21:57 +0800

    cover all case

--- src/main/java/BudgetService.java
+++ src/main/java/BudgetService.java
@@ -1,14 +1,46 @@
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class BudgetService {
 
     private final BudgetRepo repo;
+
+    DateTimeFormatter yyyyMM = DateTimeFormatter.ofPattern("yyyyMM");
 
     public BudgetService(BudgetRepo repo) {
         this.repo = repo;
     }
 
     public double query(LocalDate start, LocalDate end) {
+        if (start.isAfter(end)) {
+            return 0.0;
+        }
+
+        double budget = 0;
+
+        List<Budget> allBudget = repo.getAll();
+        for (LocalDate date = start; date.isBefore(end.plusDays(1)); date = date.plusDays(1)) {
+            String key = date.format(yyyyMM);
+            int monthBudget = getMonthBudget(key, allBudget);
+            if (monthBudget == 0) {
+                continue;
+            }
+
+            int lengthOfMonth = YearMonth.of(date.getYear(), date.getMonth()).lengthOfMonth();
+            budget += (double) monthBudget / lengthOfMonth;
+        }
+
+        return budget;
+    }
+
+    private int getMonthBudget(String key, List<Budget> allBudget) {
+        for (Budget budget : allBudget) {
+            if (budget.getYearMonth().equals(key)) {
+                return budget.getAmount();
+            }
+        }
         return 0;
     }
 }
--- src/test/java/BudgetServiceTest.java
+++ src/test/java/BudgetServiceTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -6,22 +5,70 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 class BudgetServiceTest {
 
+    private BudgetRepo mockRepo;
+    private BudgetService budgetService;
+
     @BeforeEach
     void setUp() {
+        mockRepo = Mockito.mock(BudgetRepo.class);
+        budgetService = new BudgetService(mockRepo);
     }
 
     @Test
-    void query() {
-        BudgetRepo mockRepo = Mockito.mock(BudgetRepo.class);
-        when(mockRepo.getAll()).thenReturn(Arrays.asList(
-                new Budget("202003", 3100)
-        ));
-
-        BudgetService budgetService = new BudgetService(mockRepo);
-        Assertions.assertEquals(200, budgetService.query(LocalDate.of(2020, 03, 01), LocalDate.of(2020, 03, 02)));
+    void test_start_big_than_end() {
+        LocalDate start = LocalDate.of(2020, 3, 2);
+        LocalDate end = LocalDate.of(2020, 3, 1);
+        assertEquals(0, budgetService.query(start, end));
     }
+
+    @Test
+    void test_no_month_budget() {
+        when(mockRepo.getAll()).thenReturn(Arrays.asList(
+                new Budget("202004", 3000),
+                new Budget("202006", 3000)
+        ));
+        LocalDate start = LocalDate.of(2020, 5, 1);
+        LocalDate end = LocalDate.of(2020, 5, 31);
+        assertEquals(0, budgetService.query(start, end));
+    }
+
+    @Test
+    void test_one_month_budget() {
+        when(mockRepo.getAll()).thenReturn(Arrays.asList(
+                new Budget("202004", 3000)
+        ));
+        LocalDate start = LocalDate.of(2020, 4, 1);
+        LocalDate end = LocalDate.of(2020, 4, 30);
+        assertEquals(3000.00, budgetService.query(start, end));
+    }
+
+    @Test
+    void test_partial_month_budget() {
+        when(mockRepo.getAll()).thenReturn(Arrays.asList(
+                new Budget("202004", 3000)
+        ));
+        LocalDate start = LocalDate.of(2020, 4, 10);
+        LocalDate end = LocalDate.of(2020, 4, 15);
+        assertEquals(600.00, budgetService.query(start, end));
+    }
+
+    @Test
+    void test_cross_month_budget() {
+        when(mockRepo.getAll()).thenReturn(Arrays.asList(
+                new Budget("202004", 3000),
+                new Budget("202006", 30),
+                new Budget("202007", 310),
+                new Budget("202008", 62)
+        ));
+        LocalDate start = LocalDate.of(2020, 4, 29);
+        LocalDate end = LocalDate.of(2020, 8, 2);
+        assertEquals(544.00, budgetService.query(start, end));
+    }
+
+
 }
